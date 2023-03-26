5386884d33d8b4ed71126a09806b05e140960223
joelizman <127622815+joelizman@users.noreply.github.com>
2023-03-26 08:01:21 +0100

    Delete week3Assignment.php

--- week3Assignment.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $connect = mysqli_connect("localhost", "root", "", "side_hustle");
-    if($connect){
-        echo "connected <br>";
-    }else{
-        echo "not connected";
-    }
-?>
